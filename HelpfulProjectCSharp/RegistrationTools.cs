a8934739abd3e149153a7d7993476fbd29711c89
dysha2 <dyha035@gmail.com>
2023-06-02 14:23:41 +0500

    push

--- HelpfulProjectCSharp/RegistrationTools.cs
+++ HelpfulProjectCSharp/RegistrationTools.cs
@@ -54,6 +54,27 @@ namespace HelpfulProjectCSharp
             }
             return stringBuilder.ToString();
         }
+        public static string GetPasswordSha256(string pas, string salt)
+        {
+            if (pas.Length == 0) throw new NullReferenceException();
+            return Sha256Hash(pas + salt);
+        }
+        private static string Sha256Hash(string rawData)
+        {
+            // Create a SHA256   
+            using (SHA256 sha256Hash = SHA256.Create())
+            {
+                // ComputeHash - returns byte array  
+                byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(rawData));
 
+                // Convert byte array to a string   
+                StringBuilder builder = new StringBuilder();
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    builder.Append(bytes[i].ToString("x2"));
+                }
+                return builder.ToString();
+            }
+        }
     }
 }
